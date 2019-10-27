1309325ce7be9602ea85ef836cea9e4ef7ccd367
Jeremy B. Warner <Jeremybwarner@gmail.com>
2019-10-27 12:39:11 -0700

    Update GoToScene.cs

--- GoToScene.cs
+++ GoToScene.cs
@@ -5,15 +5,19 @@ using UnityEngine.SceneManagement;
 
 public class GoToScene : MonoBehaviour
 {
-    // 1. WHAT IS THE ITEM IN LINE 9?
+    // 1. WHAT IS THE ITEM IN LINE 10?
+    // WRITE YOUR ANSWER HERE
     public string SceneName;
 
-    // 2. WHAT IS HAPPENING IN THE METHOD FROM LINE 12 thru 19?
+    // 2. WHAT IS HAPPENING IN THE METHOD FROM LINE 14 thru 23?
+    // WRITE YOUR ANSWER HERE
     private void OnTriggerEnter(Collider other)
     {
         // In the console, it will appear that the character hit the trigger saying that the character entered the public variable string name
         Debug.Log("Entered " + SceneName);
+        
         // 3. WHICH LINE IS LINE 17 GETTING THE INFORMATION FROM?
+        // WRITE YOUR ANSWER HERE
         SceneManager.LoadScene(SceneName);
     }
 }
