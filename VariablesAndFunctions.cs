fcf74ed207816eded91565f04993c9355aea3e01
Jeremy B. Warner <Jeremybwarner@gmail.com>
2019-10-27 12:16:48 -0700

    Update VariablesAndFunctions.cs

--- VariablesAndFunctions.cs
+++ VariablesAndFunctions.cs
@@ -3,16 +3,18 @@ using System.Collections;
 
 public class VariablesAndFunctions : MonoBehaviour
 {   
+
+// 1. WHAT IS THE NAME OF THE VARIABLE IN LINE 8?
     int myInt = 5;
     
-    
+// 2. DESCRIBE WHAT IS HAPPENING IN THIS METHOD.     
     void Start ()
     {
         myInt = MultiplyByTwo(myInt);
         Debug.Log (myInt);
     }
     
-    
+ // 3. WHY DOES THIS METHOD NOT HAVE THE WORD VOID?   
     int MultiplyByTwo (int number)
     {
         int ret;
