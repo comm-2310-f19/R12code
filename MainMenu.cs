602eaf7fe07d3be971ac72ce63f9c410ee992d3b
Jeremy B. Warner <Jeremybwarner@gmail.com>
2019-10-27 12:40:23 -0700

    Update MainMenu.cs

--- MainMenu.cs
+++ MainMenu.cs
@@ -1,19 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-// 1. WHY ARE WE ADDED ON LINE 5?
+// 1. WHY ARE WE ADDED ON LINE 6?
+// WRITE YOUR ANSWER HERE
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
 
-    // 2. WHAT IS HAPPENING IN THIS METHOD FROM LINE 11 thru 14?
+    // 2. WHAT IS HAPPENING IN THIS METHOD FROM LINE 13 thru 16?
+    // WRITE YOUR ANSWER HERE
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
-    // 3. WHAT IS HAPPENING IN THIS METHOD FROM LINE 17 thru 22?
+    // 3. WHAT IS HAPPENING IN THIS METHOD FROM LINE 20 thru 25?
+    // WRITE YOUR ANSWER HERE
     public void QuitGame()
     {
         Debug.Log("QUIT");
